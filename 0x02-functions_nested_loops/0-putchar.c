d11857475a10b8119d18bf0853541b940e657c10
DanMuthomi <thedanielmuthomi@gmail.com>
2022-10-18 11:10:32 +0000

    error corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main prints _putchar
+ * main - Prints Holberton as a message.
  *
- * Return is zero
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char put[] = {_, p, u, t, c, h, a, r};
-	int count, size;
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
 
-	size = sizeof(put) / sizeof(int);
-	for (count = 0; count < size; count++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
 		_putchar(str[count]);
 	}
