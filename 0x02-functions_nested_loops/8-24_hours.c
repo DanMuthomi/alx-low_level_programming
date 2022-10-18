83272624b604c512cb6f37b36bb0b5e12aa92fa6
DanMuthomi <thedanielmuthomi@gmail.com>
2022-10-18 18:42:28 +0000

    printing all minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints the minutes of a day
+*
+* Return: null
+*/
+void jack_bauer(void)
+{
+	int h1, h0, m1, mo;
+
+	for (h1 = 48; h1 <= 50; h1++)
+	{
+		for (h0 = 48; h0 <= 57; h0++)
+		{
+			for (m1 = 48; m1 <= 53; m1++)
+			{
+				for (m0 = 48; m0 <= 57; m0++)
+				{
+					if (h1 >= 50 && h0 >= 52)
+					{
+						break;
+					_putchar(h1);
+					_putchar(h0);
+					_putchar(58);
+					_putchar(m1);
+					_putchar(m0);
+					_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
