5defaf90e4de4333702190d35949b75fd4c30cb8
DanMuthomi <thedanielmuthomi@gmail.com>
2022-10-21 16:55:26 +0000

    changing algo for printing, new one prints each digit separately

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,17 @@ void more_numbers(void)
 	int i = 0;
 	int j;
 
-	while (i <= 9)
+	while (i < 10)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(" %d ", j);
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48;
+			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar("\n");
 		i++;
-	_putchar("\n");
 	}
 }
