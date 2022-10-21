c5270d49ba7b74a359b9f5d7dd86aef48f341b5c
DanMuthomi <thedanielmuthomi@gmail.com>
2022-10-21 19:32:26 +0000

    printing # in a square pattern

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_square - prints a square using #
+*
+* @size: shows the size of square
+*
+* No return
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar("#");
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
