f3eb816afadeb12d12b6677812824d48739e957a
DanMuthomi <thedanielmuthomi@gmail.com>
2022-10-29 19:41:35 +0000

    error correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,28 +10,27 @@
 
 char *cap_string(char *str)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int conversion, index, count;
+	char chars[] = {' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}',
+			 '\t', '\n', '\0'};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
+	conversion = 32;
+
+	for (index = 0; str[index] != '\0'; index++)
 	{
-		*(s + count) = *(s + count) - 32;
-	}
-	count++;
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 13; i++)
+		if (str[index] >= 'index' && str[index] <= 'z')
 		{
-			if (*(s + count) == sep_words[i])
+			str[index] =  str[index] - conversion;
+		}
+		conversion = 0;
+		for (count = 0; chars[count] != '\0'; count++)
+		{
+			if (chars[count] == str[index])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-				{
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				}
+				conversion = 32;
 				break;
 			}
 		}
-		count++;
 	}
-	return (s);
+	return (str);
 }
