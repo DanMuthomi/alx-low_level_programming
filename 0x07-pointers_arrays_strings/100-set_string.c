7a756baa89b07eb00c339c2959f766b6116ff09c
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-01 20:43:53 +0000

    setting pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* set_string - setting pointer value to char
+*
+* @s: source addrress
+* @to: target addrress
+*
+* No return
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
