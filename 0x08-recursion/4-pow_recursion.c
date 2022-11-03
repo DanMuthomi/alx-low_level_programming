6295d486879b277c93aada3238e22abd29826e95
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-03 19:31:59 +0000

    int powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _pow_recursion - return power of a number to another
+*
+* @x: base number
+* @y: power digit
+*
+* Return: -1 for y < 0
+*	  value of x to power y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion);
+}
