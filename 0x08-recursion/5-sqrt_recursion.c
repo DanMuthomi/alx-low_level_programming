63033c67a2874afeb2e8ba21238b1a918e0d1586
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-05 02:58:38 +0000

    return descriptor

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-* power_operation - returns natural square root of number
+* power_check - returns natural square root of number
 *
 * @n: input int
 * @c: iterator
 *
-* return: square root
+* Return: -1
+*	  root if true
 */
 
-int power_operation(int n, int c)
+int power_check(int n, int c)
 {
 	if (c % (n / c) == 0)
 	{
@@ -18,7 +19,7 @@ int power_operation(int n, int c)
 		else
 			return (-1);
 	}
-	return (0 + power_operation(n, c + 1));
+	return (0 + power_check(n, c + 1));
 }
 
 /**
@@ -38,5 +39,5 @@ int _sqrt_recursion(int n)
 		return (0);
 	if (n == 1)
 		return (1);
-	return (power_operation(n, 2));
+	return (power_check(n, 2));
 }
