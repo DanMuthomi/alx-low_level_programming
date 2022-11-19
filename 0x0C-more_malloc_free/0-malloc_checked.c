7daa5b1c925d45e7a181e83c0d6e8dc098777f10
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-19 16:42:25 +0000

    pointer to allocated memory address

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - gives pointer to allocated memory
+*
+* @b: no of bytes
+*
+* Return: pointer
+* if fail, return status value of 98
+*/
+
+void *malloc_checked(insigned int b)
+{
+	char *c;
+
+	c = malloc(b);
+	if (c == NULL)
+		exit(98);
+	return (c);
+}
