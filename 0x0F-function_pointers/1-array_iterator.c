c59fdefb755ed1a2d227d0237b7030ce9378305d
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-20 02:21:06 +0000

    syntax error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int index;
 
-	if (array != NULL && size > 0 && action != NULL)
-		for (index = 0; index < size; index++)
-			action(array[index]);
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
