36dfe01b45843c21346eb0eed6722e9f5f7bb524
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-28 21:47:08 +0000

    comment tweaks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -29,12 +29,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = str_len;
 	new_node->next = NULL;
 
-	/*copy our starting node pointer to temp*/
 	temp = *head;
 
-	/*if temp is NULL place our new_node at the beginning of the list*/
-	/*else if its not null we place our new_node to temp->next hence*/
-	/*adding our new_node at the end of the list*/
 	if (temp == NULL)
 		*head = new_node;
 	else
