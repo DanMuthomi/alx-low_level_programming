66429c983e9c08ccb25b2a66d2bfa2c6fc5f33b5
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-28 21:23:39 +0000

    tweaks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,28 +9,23 @@
 * Return: starting address of list
 *	  NULL if fail
 */
-list_t *add_node(lists_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	lists_t *new_node;
-	size_t str_len = 0;
+	list_t *new;
+	size_t nchar;
 
-	if (str == NULL)
-		str_len = 0;
-	while (str[str_len] != '\0')
-		str_len++;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	if (*head == NULL)
-		new_node->next = NULL;
-	else
-		new_node->next = *head;
+	new->str = strdup(str);
 
-	new_node->str = strdup(str);
-	new_node->len = str_len;
-	*head = new_node;
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
 	return (*head);
 }
