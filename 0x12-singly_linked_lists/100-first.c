d2ba5010fe064afb1098118aa269a109b537cdad
DanMuthomi <thedanielmuthomi@gmail.com>
2022-11-28 22:02:12 +0000

    code tweaks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-* prnmain - printing lines before running main
+* amain - function executed before main
 *
-* No return
+* Return: no return
 */
-void __attribute__ ((cinstructor)) prnmain()
+
+void __attribute__ ((constructor)) amain()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
